b309bca3fc28ca44c9e312642860359cf32bac8f
coelitherese <ctg-brillante.student@ua.edu.ph>
2025-08-29 17:59:11 +0800

    Aug 29 2025

---
+++ Aug 29 2025/myWeb1.js
@@ -0,0 +1,21 @@
+$fname="Smooth"
+var $lname = "Operator"
+let $mySalary = 25000;
+    $mySalary = 50000;
+let $address = "Spain";
+const pi = 3.14;
+
+$fname2="Max"
+var $lname2 = "Verstappen"
+
+console.log("My name is " + $lname + "," + " " + $fname + " " + $lname);
+console.log("Hello, " + $fname + " " + $lname + " how are you today? Podium finish pls");
+console.log("TU TU TU DU " + $fname2 + " " + $lname2);
+
+
+console.log("My salary is " + $mySalary);
+document.getElementById("myAddress").innerHTML=$address;
+
+console.log("Hello, World!");
+console.log("I am learning JavaScript.");
+console.log("The value of pi is " + pi);
---
+++ Aug 29 2025/myWeb2.js
@@ -0,0 +1,17 @@
+let $name = "Jayson";
+let $name2 = "Colin";
+let $drink = "cofee";
+let $drink2 = "tea"
+let $drinkSize = "large";
+let $drinkSize2 = "medium";
+let $price = 10;
+
+console.log("Hello my friend" + $name + ", how is your day?");
+console.log("I am ok" + $name2);
+console.log("How about you" + $name);
+console.log("Well I need a cup of" + $drink + " to start my day");
+console.log("Do you want a cup of " + $drink2 + "?");
+console.log("Yes please! I want the " + $drinkSize + "one")
+console.log("Well they only serves the " + $drinkSize2 + "one");
+console.log("That's ok, here is my " + $price + "dollars");
+console.log("Thank you");
