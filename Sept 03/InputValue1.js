1c6ee9b7a999cd9db9fce64bbe7b34696af59ac4
Coeli Therese <liyannaskylene@gmail.com>
2025-09-03 17:59:37 +0800

    Sept 03

---
+++ Sept 03/InputValue1.js
@@ -0,0 +1,14 @@
+console.log ("This topic will allow us to input values");
+console.log ("");
+
+// let $num1 = parseFloat (prompt("Please enter your first number: "));
+// let $num2 = parseFloat (prompt("Please enter your second number: "));
+
+// let $sum = $num1 + $num2;
+// let $prod = $num1 * $num2;
+// let $ave = ($num1+$num2)/2;
+
+
+console.log ("The SUM of " + $num1 + " and " + $num2 + " is " + $sum);
+console.log ("The PRODUCT of " + $num1 + " and " + $num2 + " is " + $prod);
+console.log("The AVERAGE of the given numbers: " + $num1 + ", " + $num2 + " is " + $ave);
